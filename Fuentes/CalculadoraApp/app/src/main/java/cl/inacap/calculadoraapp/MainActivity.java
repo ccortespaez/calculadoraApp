594ee84fc419cb73cd0a0601ef912723ddfb115b
Cristobal Cortés <cristobal.cortespaez@gmail.com>
2020-09-12 16:27:51 -0300

    Agregar la base de la calculadora
    Obtener los datos en los EditText en el activity
    Generar layout del activity completo
    Agregar el color a los botones
    Agrear ImagenView para la calculadora
    Agregar funcionalidad de la calculadora

--- Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraapp/MainActivity.java
+++ Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraapp/MainActivity.java
@@ -3,12 +3,40 @@ package cl.inacap.calculadoraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText n1Txt;
+    private EditText n2Txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.n1Txt = findViewById(R.id.n1Txt);
+        this.n2Txt = findViewById(R.id.n2Txt);
+    }
+
+    public void sumar(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La suma es "+(n1+n2), Toast.LENGTH_SHORT).show();
+    }
+    public void restar(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La resta es "+(n1-n2), Toast.LENGTH_SHORT).show();
+    }
+    public void multiplicar(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La multiplicacion es "+(n1*n2), Toast.LENGTH_SHORT).show();
+    }
+    public void dividir(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La divicion es "+(n1/n2), Toast.LENGTH_SHORT).show();
     }
 }
